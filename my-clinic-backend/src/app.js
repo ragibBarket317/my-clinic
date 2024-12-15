e78675d9a3972a6d194a8947728b9797d1af8d6a
Ragib Barket <ragibrahman86317@gmail.com>
2024-12-15 18:39:42 +0600

    cors

--- my-clinic-backend/src/app.js
+++ my-clinic-backend/src/app.js
@@ -46,7 +46,12 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
